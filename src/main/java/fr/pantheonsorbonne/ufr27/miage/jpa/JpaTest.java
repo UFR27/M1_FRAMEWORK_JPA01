33949aeb0519fd44bd58153a7e0ccc9137d0148b
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2019-08-19 15:28:05 +0200

    added hibernate in pom

--- src/main/java/fr/pantheonsorbonne/ufr27/miage/jpa/JpaTest.java
+++ src/main/java/fr/pantheonsorbonne/ufr27/miage/jpa/JpaTest.java
@@ -12,33 +12,25 @@ public class JpaTest {
 	@Inject
 	private EntityManager manager;
 
-	
-	
-	
 	public static void main(String[] args) throws ClassNotFoundException {
 
-		
 		SeContainerInitializer initializer = SeContainerInitializer.newInstance();
 
-		try (SeContainer container = initializer.addPackages(true,JpaTest.class.getPackage()).initialize()) {
+		try (SeContainer container = initializer.addPackages(true, JpaTest.class.getPackage()).initialize()) {
 			JpaTest jpa = container.select(JpaTest.class).get();
 			EntityTransaction tx = jpa.manager.getTransaction();
 			tx.begin();
 			try {
-				
+
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			tx.commit();
-	
-			
-	
+
 			System.out.println(".. done");
-			
+
 		}
 
 	}
-
-	
 
 }
